4616970f6c1649e6e8399c6ae602fa24f94e382a
Viraj-Chauhan <viraj.chauhan@brainvire.com>
2024-05-14 19:12:02 +0530

    Request & response Throttle

--- restapp/urls.py
+++ restapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.helloworld,name='home')
+    path('',views.helloworld,name='home'),
+    path('h1/',views.hello_world,name='home1'),
+    path('v1/',views.view,name='view')
 ]
--- restapp/views.py
+++ restapp/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view ,throttle_classes
 from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.throttling import UserRateThrottle
 from rest_framework import status
 # Create your views here.
 
 @api_view()
 def helloworld(request):
-    return Response({"message":"Hello,World"})
+    return Response({"message":"Hello,World"},status.HTTP_200_OK)
+
+@api_view(['GET', 'POST'])
+def hello_world(request):
+    if request.method == 'POST':
+        return Response({"message": "Got some data!", "data": request.data})
+    return Response({"message": "Hello, world!"})
+
+class OncePerDayUserThrottle(UserRateThrottle):
+    rate = '5/day'
+
+@api_view(['GET'])
+@throttle_classes([OncePerDayUserThrottle])
+def view(request):
+    return Response({"message": "Hello for today! See you tomorrow!"})
