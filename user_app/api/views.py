2c55814e7af4c5d00117abe4e78d730b74fedd10
Viraj-Chauhan <viraj.chauhan@brainvire.com>
2024-05-25 16:03:11 +0530

    logout

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view ,authentication_classes
+from rest_framework.decorators import api_view ,authentication_classes , permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from .serializers import RegisterSerializer
 from rest_framework.authtoken.models import Token
 from rest_framework import status
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.views import APIView
+from rest_framework.authentication import TokenAuthentication, BaseAuthentication
 
+class Noauthentication(BaseAuthentication):
+    def authenticate(self, request):
+        return None
 
 @api_view(['POST',])
 @authentication_classes([TokenAuthentication])
@@ -15,10 +20,24 @@ def logout_view(request):
         return Response(status=status.HTTP_200_OK)
 
 # Create your views here.
-@api_view(['POST',])
-def registration_view(request):
-    if request.method == 'POST':
-        serializer =RegisterSerializer(data=request.data)
-        if serializer.is_valid():
+# @api_view(['POST',])
+# @authentication_classes([Noauthentication])
+# @permission_classes([AllowAny])
+# def registration_view(request):
+#     if request.method == 'POST':
+#         serializer = RegisterSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({'message':'User Created'})
+        
+class Registration_view(APIView):
+    # authentication_classes=[Noauthentication]
+    permission_classes=[AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
-            return Response(serializer.data)
+            return Response({'message':'User Created'},status=status.HTTP_201_CREATED)
+        
+        
